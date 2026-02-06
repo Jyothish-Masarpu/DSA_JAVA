1bff94debc4e50fda712f2308d457c123b60aa39
Jyothish <jyothishmasarpu@gmail.com>
2026-02-06 18:59:30 +0530

    Added Twosum program

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,25 @@
+/*
+LeetCode 1: Two Sum
+Given an array of integers nums and an integer target, return 
+indices of the two numbers such that they add up to target.
+*/
+import java.util.*;
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        int target=9;
+        int[] res=twoSum(nums,target);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{0};
+    }
+}
